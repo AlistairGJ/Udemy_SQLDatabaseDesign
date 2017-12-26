4bcff8be7cd47955b516f7ee9d2d0f6dcccf3857
AlistairGJ <AlistairGJ@gmail.com>
2017-12-26 22:44:19 +1100

    update

--- SampleSubmission.sql
+++ SampleSubmission.sql
@@ -10,7 +10,7 @@ DROP TABLE IF EXISTS RequiredAnalysis;
 DROP TABLE IF EXISTS ProjectLead;
 
 CREATE TABLE Project(
-	ProjectID				INTEGER,
+	ProjectID					INTEGER,
     	PRIMARY KEY (ProjectID)
 );
 
@@ -19,27 +19,27 @@ CREATE TABLE Project(
 -- If want drop-downs for Building / Room, will need more relations to fill these feilds
 -- For units, must state this as an assumption (and use same throughout the DB) or add to name of attribute
 CREATE TABLE TCU(
-	TCU_ID					INTEGER,
-    	Building				VARCHAR(20),
-   	Room					VARCHAR(20),
-   	Temperature_celcius		NUMERIC, 
+	TCU_ID						INTEGER,
+    Building					VARCHAR(20),
+   	Room						VARCHAR(20),
+   	Temperature_celcius			NUMERIC, 
    	PRIMARY KEY (TCU_ID)
 );
 
 CREATE TABLE Department(
-	DepartmentID			VARCHAR(5),
-    	DepartmentName			VARCHAR(30),
+	DepartmentID				VARCHAR(5),
+   	DepartmentName				VARCHAR(30),
    	PRIMARY KEY (DepartmentID)
 );
 
 CREATE TABLE Sample(
-	SampleID				SERIAL,
-    	LotNumber				VARCHAR(80),
+	SampleID					SERIAL,
+    LotNumber					VARCHAR(80),
    	ExperimentNumber			VARCHAR(80),
-   	BatchNumber				VARCHAR(80),
-   	ProcessStep				VARCHAR(80),
-   	ProjectID				INTEGER,
-   	TCU_ID					INTEGER,
+   	BatchNumber					VARCHAR(80),
+   	ProcessStep					VARCHAR(80),
+   	ProjectID					INTEGER,
+   	TCU_ID						INTEGER,
    	DepartmentID				CHAR(3),
    	PRIMARY KEY (SampleID),
    	FOREIGN KEY (ProjectID) REFERENCES Project (ProjectID),
@@ -51,13 +51,13 @@ CREATE TABLE Sample(
 
 
 CREATE TABLE Employee(
-	EmployeeID				VARCHAR(7),
-   	FirstName				VARCHAR(80),
-   	LastName				VARCHAR(80),
-   	Email					VARCHAR(254), 
-   	ManagerID				VARCHAR(7),
+	EmployeeID					VARCHAR(7),
+   	FirstName					VARCHAR(80),
+   	LastName					VARCHAR(80),
+   	Email						VARCHAR(254), 
+   	ManagerID					VARCHAR(7),
    	DepartmentID				VARCHAR(5),
-    	PRIMARY KEY (EmployeeID),
+    PRIMARY KEY (EmployeeID),
    	FOREIGN KEY (ManagerID) REFERENCES Employee,
    	FOREIGN KEY (DepartmentID) REFERENCES Department
 );
@@ -70,45 +70,45 @@ CREATE TABLE Submission(
    	SubmissionID				SERIAL,
   	NumberOfVials				INTEGER,
   	DateSubmitted				DATE,
-    	DueDate					DATE,
-    	Volume_microLitre			NUMERIC,
-    	UserComment				VARCHAR(1024),
+    DueDate						DATE,
+    Volume_microLitre			NUMERIC,
+    UserComment					VARCHAR(1024),
    	ProteinConc_mgPerL			NUMERIC,
-    	EmployeeID				VARCHAR(7),
-    	SampleID				INTEGER, 
-    	PRIMARY KEY (SubmissionID),
+    EmployeeID					VARCHAR(7),
+    SampleID					INTEGER, 
+    PRIMARY KEY (SubmissionID),
    	FOREIGN KEY (EmployeeID) REFERENCES Employee,
-    	FOREIGN KEY (SampleID) REFERENCES Sample    
+    FOREIGN KEY (SampleID) REFERENCES Sample    
 );
 
 CREATE TABLE Analysis(
-    	AnalysisID				VARCHAR(40),
-    	Description				VARCHAR(1024),
-    	ResultUnits				VARCHAR(255),  
-    	PRIMARY KEY (AnalysisID)
+    AnalysisID					VARCHAR(40),
+   	Description					VARCHAR(1024),
+    ResultUnits					VARCHAR(255),  
+    PRIMARY KEY (AnalysisID)
 );
 
 -- Function for TAT, part of a query
 CREATE TABLE AnalysisResult(
-	AnalysisID				VARCHAR(40),
+	AnalysisID					VARCHAR(40),
    	SubmissionID				INTEGER,
-   	ResultValue				NUMERIC,
+   	ResultValue					NUMERIC,
    	PRIMARY KEY (AnalysisID, SubmissionID),
    	FOREIGN KEY (AnalysisID) REFERENCES Analysis, 
-    	FOREIGN KEY (SubmissionID) REFERENCES Submission
+    FOREIGN KEY (SubmissionID) REFERENCES Submission
 );
 
 CREATE TABLE RequiredAnalysis(
-	SampleID				INTEGER,
-   	AnalysisID				VARCHAR(40),
+	SampleID					INTEGER,
+   	AnalysisID					VARCHAR(40),
    	PRIMARY KEY (SampleID, AnalysisID),
    	FOREIGN KEY (SampleID) REFERENCES Sample,
    	FOREIGN KEY (AnalysisID) REFERENCES Analysis   
 );
 
 CREATE TABLE ProjectLead(
-	ProjectID				INTEGER,
-   	EmployeeID				VARCHAR(40),
+	ProjectID					INTEGER,
+   	EmployeeID					VARCHAR(40),
    	PRIMARY KEY (ProjectID, EmployeeID),
    	FOREIGN KEY (ProjectID) REFERENCES Project,
    	FOREIGN KEY (EmployeeID) REFERENCES Employee   
